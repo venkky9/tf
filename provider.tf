c2929e2e76475d4546e00104128aebe2fe0fa797
venkky9 <62655111+venkky9@users.noreply.github.com>
2020-08-02 19:48:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
-  version = "~> 2.54"
+  region     = "us-east-1"
   access_key = "AKIA3NX7CK4TKVHD6456"
   secret_key = "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"UPcSHKIYotbms1Bqgq9i9W6Hf9nNBo38rGC4NoUK"
 
